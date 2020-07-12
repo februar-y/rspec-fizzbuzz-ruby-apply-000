df75ef86938b980a565688fda44f49d5a88ca1c2
rational-architecture-1641 <jarmah241@gmail.com>
2020-07-12 22:55:55 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -4,11 +4,5 @@
 def fizzbuzz(num)
   if num % 3 == 0 and num % 5 == 0
     puts "FizzBuzz"
-  elsif num % 3 == 0
-    puts "Fizz"
-  elsif num % 5 == 0
-    puts "Buzz"
-  else
-    puts nil
   end
 end
